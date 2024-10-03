8ea473be408682b9f16e08160a0089924c1c48da
Ferron503 <62104059+ferron0503@users.noreply.github.com>
2024-10-03 20:55:52 +0900

    Update projecteuler_5.hs

--- Archived Problems/projecteuler_5.hs
+++ Archived Problems/projecteuler_5.hs
@@ -2,3 +2,4 @@ main :: IO ()
 main = do
     let n = (2 ^ 4) * (3 ^ 2) * 5 * 7 * 11 * 13 * 17 * 19 --can do it with your hands
     print n
+    --I know this solution is Ad_hoc, but I could think this solution only.
